ceac91953aae35d3ab64f18db50dc54008ee891d
je355 <chaeeehui@gmail.com>
2024-04-26 15:21:02 +0900

    threejs 공부하기~

---
+++ threejs/src/App.js
@@ -0,0 +1,22 @@
+import './App.css'
+import React from 'react';
+import { Canvas } from '@react-three/fiber';
+import { OrbitControls } from '@react-three/drei';
+
+function App() {
+  return (
+    <>
+      <Canvas>
+        <OrbitControls autoRotate={true}/>
+        <mesh>
+          <ambientLight intensity={1} />
+          <directionalLight position={[-1,0,1]} intensity={0.5} />
+          <boxGeometry args={[1, 1, 1]} />
+          <meshStandardMaterial attach="material" color={0xa3b18a}/>
+        </mesh>
+      </Canvas>
+    </> 
+  );
+}
+
+export default App;
